d7b8bdb16776dda3e9997bbd77ecc99e298c04d6
sametbakar <sametb.3230@gmail.com>
2022-08-28 17:30:28 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        List<Customer> customers = new List<Customer>();
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi!");
+            Console.WriteLine(customer.FirstName + " " + customer.SecondName);
+            Console.WriteLine("-----------------------");
+            customers.Add(customer);
+        }
+        public void List()
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.FirstName + " " + customer.SecondName);
+            }
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.SecondName + " adlı müşteri silindi!");
+            customers.Remove(customer);
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Ali";
+            customer1.SecondName = "Korkmaz";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 1;
+            customer2.FirstName = "Ayşe";
+            customer2.SecondName = "Gül";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 2;
+            customer3.FirstName = "Ahmet";
+            customer3.SecondName = "Seven";
+
+
+
+            CustomerManager manager = new CustomerManager();
+
+            manager.Add(customer1);
+            manager.Add(customer2);
+            manager.Add(customer3);
+
+            manager.List();
+            Console.WriteLine("-----------------------");
+            manager.Delete(customer2);
+            Console.WriteLine("-----------------------");
+            manager.List();
+        }
+    }
+}
